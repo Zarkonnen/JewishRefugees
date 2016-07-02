1f299897bcee213749eeac5db864973cfb7d4fc4
Zarkonnen <david.stark@zarkonnen.com>
2016-07-02 15:06:23 +0200

    JSON info for famous people and also icons and images.

--- geocode.py
+++ geocode.py
@@ -8,7 +8,7 @@ for source_name in sys.argv[2:]:
         for l in f:
             required_locations.add(l.split("\t")[1])
 
-with open("geocodes_switzerland.tsv") as f:
+with open("geocodes.tsv") as f:
     for l in f:
         location = l.split("\t")[0]
         if location in required_locations:
@@ -17,18 +17,27 @@ with open("geocodes_switzerland.tsv") as f:
 if mode == "code":
     print "Geocoding", len(required_locations), "new locations"
 
-    with open("geocodes_switzerland.tsv", "a") as f:
+    with open("geocodes.tsv", "a") as f:
         for l in required_locations:
             print "Geocoding", l
-            r = requests.get("https://maps.googleapis.com/maps/api/geocode/json", params={"address":l + ", Switzerland", "key":"AIzaSyCQ8YLJ3J5CcGUBB9t9SdL-bSJRrbD_3po"})
-            try:
-                lat = r.json()["results"][0]["geometry"]["location"]["lat"]
-                lng = r.json()["results"][0]["geometry"]["location"]["lng"]
-                f.write(l + "\t" + str(lat) + "\t" + str(lng) + "\n")
-                print "Found!"
-            except:
-                print "Unable to geocode!"
+            variations = [
+                #l.strip(),
+                #l.strip().split("/")[0].split(",")[0].split("-")[0].strip(),
+                l.strip().split("/")[0].split(",")[0].split("-")[0].strip().split(" ")[-1],
+                l.strip().split("/")[0].split(",")[0].split("-")[0].strip().split(" ")[0],
+            ]
+            for v in variations:
+                r = requests.get("https://maps.googleapis.com/maps/api/geocode/json", params={"address":v.replace("/", ","), "key":"AIzaSyCQ8YLJ3J5CcGUBB9t9SdL-bSJRrbD_3po"})
+                try:
+                    lat = r.json()["results"][0]["geometry"]["location"]["lat"]
+                    lng = r.json()["results"][0]["geometry"]["location"]["lng"]
+                    f.write(l + "\t" + str(lat) + "\t" + str(lng) + "\n")
+                    print "Found!"
+                    break
+                except:
+                    print "Unable to geocode trying " + v                
 
 if mode == "list":
     for l in required_locations:
         print l
+    print len(required_locations)
